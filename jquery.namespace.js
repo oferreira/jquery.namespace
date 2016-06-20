e9c459dd48a77d19401f328f90424716fc785dbf
oferreira <olivier@mediainvest.io>
2016-06-20 18:46:49 +0200

    Update jquery.namespace.js

--- jquery.namespace.js
+++ jquery.namespace.js
@@ -1,22 +1,20 @@
-(function ($) {
-    $.namespace = function (namespaceName, closures) {
+$.namespace = function (namespaceName, closures) {
 
-        if ($.fn[namespaceName] === undefined) {
-            $.fn[namespaceName] = function executor(context) {
-                if (this instanceof executor) {
-                    this.__context__ = context;
-                }
-                else {
-                    return new executor(this);
-                }
-            };
-        }
+    if ($.fn[namespaceName] === undefined) {
+        $.fn[namespaceName] = function executor(context) {
+            if (this instanceof executor) {
+                this.__context__ = context;
+            }
+            else {
+                return new executor(this);
+            }
+        };
+    }
 
-        $.each(closures, function (closureName, closure) {
-            $.fn[namespaceName].prototype[closureName] = function () {
-                return closure.apply(this.__context__, arguments);
-            };
-        });
+    $.each(closures, function (closureName, closure) {
+        $.fn[namespaceName].prototype[closureName] = function () {
+            return closure.apply(this.__context__, arguments);
+        };
+    });
 
-    };
-})(jQuery);
+};
